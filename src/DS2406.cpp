625b177abf2ceff949b1c8b0f43b9ebadaeeb471
Georg Bauer <bauer@rohmann.de>
2023-05-31 12:22:14 +0200

    first working DS2406 memory

--- src/DS2406.cpp
+++ src/DS2406.cpp
@@ -7,15 +7,12 @@ DS2406::DS2406(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, 
     static_assert(sizeof(scratchpad) < 256, "Implementation does not cover the whole address-space");
     static_assert(sizeof(memory) < 256,  "Implementation does not cover the whole address-space");
 
+    clearStatus();
     clearMemory();
+
     clearScratchpad();
 
-    page_protection = 0;
-    page_eprom_mode = 0;
-
-    updatePageStatus();
-
-    // Pin State Section
+    // Pin State Section //???
     pin_state[0] = false;
     pin_latch[0] = false;
     pin_state[1] = false;
@@ -24,17 +21,139 @@ DS2406::DS2406(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, 
 
 void DS2406::duty(OneWireHub * const hub)
 {
-    constexpr uint8_t ALTERNATING_10 { 0xAA };
     static uint16_t   reg_TA         { 0 }; // contains TA1, TA2
-    static uint8_t    reg_ES         { 0 }; // E/S register
+    uint16_t          reg_RA         { 0 }; // target address
     uint16_t          crc            { 0 };
+    uint8_t           cmd, data;            // command, data, crc
 
-    uint8_t  page_offset { 0 };
-    uint8_t  cmd, data;
+    // always receives cmd and TA
     if (hub->recv(&cmd,1,crc))  return;
+    if (hub->recv(reinterpret_cast<uint8_t *>(&reg_TA),2,crc))  return;
 
     switch (cmd)
     {
+        case 0xF0:      // READ MEMORY // copied from DS2506
+
+            while (reg_TA <= MEM_SIZE)
+            {
+                const uint16_t destin_TA = translateRedirection(reg_TA);
+                const uint8_t  length    = PAGE_SIZE - uint8_t(reg_TA & PAGE_MASK);
+
+                if (destin_TA < MEM_SIZE)
+                {
+                    if (hub->send(&memory[destin_TA],length,crc)) return;
+                }
+                else // fake data
+                {
+                    data  = 0x00;
+                    uint8_t counter = length;
+                    while (counter-- > 0)
+                    {
+                        if (hub->send(&data, 1, crc)) return;
+                    }
+                }
+
+                reg_TA += length;
+            }
+            crc = ~crc; // normally crc16 is sent ~inverted
+            hub->send(reinterpret_cast<uint8_t *>(&crc),2);
+            break; // datasheet says we should return 1s, till reset, nothing to do here
+
+        case 0xA5:      // EXTENDED READ MEMORY (with redirection-information) // copied from DS2506
+
+            while (reg_TA <= MEM_SIZE)
+            {
+                const uint8_t  source_page = static_cast<uint8_t>(reg_TA >> 5);
+                const uint8_t  destin_page = getPageRedirection(source_page);
+                if (hub->send(&destin_page,1,crc))                        return;
+                crc = ~crc; // normally crc16 is sent ~inverted
+                hub->send(reinterpret_cast<uint8_t *>(&crc),2); // send crc of (cmd,TA,destin_page) at first, then only crc of (destin_page)
+                crc=0;
+                const uint16_t destin_TA   = translateRedirection(reg_TA);
+                const uint8_t  length      = PAGE_SIZE - uint8_t(reg_TA & PAGE_MASK);
+
+                if (destin_TA < MEM_SIZE)
+                {
+                    if (hub->send(&memory[destin_TA],length,crc)) return;
+                }
+                else // fake data
+                {
+                    data  = 0x00;
+                    uint8_t counter = length;
+                    while (counter-- != 0)
+                    {
+                        if (hub->send(&data, 1, crc)) return;
+                    }
+                }
+
+                crc = ~crc; // normally crc16 is sent ~inverted
+                if (hub->send(reinterpret_cast<uint8_t *>(&crc),2)) break;
+                reg_TA += length;
+                crc=0;
+            }
+            break; // datasheet says we should return 1s, till reset, nothing to do here
+
+        case 0x0F:      // WRITE MEMORY // copied from DS2506
+
+            while (reg_TA < MEM_SIZE) // check for valid address
+            {
+                if (hub->recv(&data,1,crc)) break;
+
+                crc = ~crc; // normally crc16 is sent ~inverted
+                if (hub->send(reinterpret_cast<uint8_t *>(&crc), 2)) break;
+                // master issues now a 480us 12V-Programming Pulse -> advantage for us, enough time to handle addressMapping
+
+                reg_RA = translateRedirection(reg_TA);
+                const uint8_t  page = static_cast<uint8_t>(reg_RA>>5);
+                if (getPageProtection(page))
+                {
+                    const uint8_t mem_zero = 0x00;
+                    if (hub->send(&mem_zero)) break;
+                }
+                else
+                {
+                    memory[reg_RA] &= data; // like EEPROM-Mode
+                    setPageUsed(page);
+                    if (hub->send(&memory[reg_RA])) break;
+                }
+                crc = ++reg_TA; // prepare new loop
+            }
+            break;
+
+        case 0x55:      // WRITE STATUS // copied from DS2506
+
+            while (reg_TA < STATUS_SIZE) // check for valid address
+            {
+                if (hub->recv(&data,1,crc)) break;
+
+                crc = ~crc; // normally crc16 is sent ~inverted
+                if (hub->send(reinterpret_cast<uint8_t *>(&crc), 2)) break;
+                // master issues now a 480us 12V-Programming Pulse
+
+                data = writeStatus(reg_TA, data);
+                if (hub->send(&data)) break;
+                crc = ++reg_TA; // prepare new loop
+            }
+            break;
+
+        case 0xAA:      // READ STATUS // copied from DS2506
+
+            while (reg_TA < STATUS_SIZE) // check for valid address
+            {
+                reg_RA = reg_TA & uint8_t(7);
+                while (reg_RA < 8)
+                {
+                    data = readStatus(reg_TA); // read byte by byte
+                    if (hub->send(&data, 1, crc)) return;
+                    reg_RA++;
+                    reg_TA++;
+                }
+                crc = ~crc; // normally crc16 is sent ~inverted
+                hub->send(reinterpret_cast<uint8_t *>(&crc),2);
+                crc = 0;
+            }
+            break;
+        /*
         case 0x0F:      // WRITE SCRATCHPAD COMMAND
 
             if (hub->recv(reinterpret_cast<uint8_t *>(&reg_TA),2,crc))  return;
@@ -80,7 +199,7 @@ void DS2406::duty(OneWireHub * const hub)
 
             {   // send Scratchpad content
                 const uint8_t start  = uint8_t(reg_TA) & SCRATCHPAD_MASK;
-                const uint8_t length = (reg_ES & SCRATCHPAD_MASK)+ uint8_t(1) - start; // difference to ds2433
+                const uint8_t length = (reg_ES & SCRATCHPAD_MASK)+ uint8_t(1) - start; // based on ds2413: difference to ds2433
                 if (hub->send(&scratchpad[start],length,crc))   return;
             }
 
@@ -129,37 +248,39 @@ void DS2406::duty(OneWireHub * const hub)
             if (reg_TA >= MEM_SIZE) return;
             if (hub->send(&memory[reg_TA],MEM_SIZE - uint8_t(reg_TA),crc)) return;
             break; // send 1s when read is complete, is passive, so do nothing here
-
+        */
         default:
 
             hub->raiseSlaveError(cmd);
     }
 }
 
-void DS2406::clearMemory(void)
+// Memory Section
+
+void DS2406::clearScratchpad(void) // copied from 
 {
-    memset(memory, static_cast<uint8_t>(0x00), sizeof(memory));
+    memset(scratchpad, static_cast<uint8_t>(0x00), SCRATCHPAD_SIZE);    // ????
 }
 
-void DS2406::clearScratchpad(void)
+void DS2406::clearMemory(void) // copied from DS2506
 {
-    memset(scratchpad, static_cast<uint8_t>(0x00), SCRATCHPAD_SIZE);
+    memset(memory, value_xFF, MEM_SIZE);
 }
 
-bool DS2406::writeMemory(const uint8_t* const source, const uint8_t length, const uint8_t position)
+bool DS2406::writeMemory(const uint8_t* const source, const uint8_t length, const uint8_t position) // copied from DS2502/DS2506
 {
-    for (uint8_t i = 0; i < length; ++i) {
-        if ((position + i) >= sizeof(memory)) break;
-        if (getPageProtection(position + i)) continue;
-        memory[position + i] = source[i];
-    }
+    if (position >= MEM_SIZE) return false;
+    const uint16_t _length = (position + length >= MEM_SIZE) ? (MEM_SIZE - position) : length;
+    memcpy(&memory[position],source,_length);
 
-    if ((position+length) > 127) updatePageStatus();
+    const uint8_t page_start = static_cast<uint8_t>(position >> 5);
+    const uint8_t page_stop  = static_cast<uint8_t>((position + _length) >> 5);
+    for (uint8_t page = page_start; page <= page_stop; page++) setPageUsed(page);
 
-    return true;
+    return (_length==length);
 }
 
-bool DS2406::readMemory(uint8_t* const destination, const uint16_t length, const uint16_t position) const
+bool DS2406::readMemory(uint8_t* const destination, const uint16_t length, const uint16_t position) const // copied from DS2431/DS2502/DS2506
 {
     if (position >= MEM_SIZE) return false;
     const uint16_t _length = (position + length >= MEM_SIZE) ? (MEM_SIZE - position) : length;
@@ -167,119 +288,69 @@ bool DS2406::readMemory(uint8_t* const destination, const uint16_t length, const
     return (_length==length);
 }
 
-void DS2406::setPageProtection(const uint8_t position)
-{
-    if      (position < 1*PAGE_SIZE)    memory[0x80] = WP_MODE;
-    else if (position < 2*PAGE_SIZE)    memory[0x81] = WP_MODE;
-    else if (position < 3*PAGE_SIZE)    memory[0x82] = WP_MODE;
-    else if (position < 4*PAGE_SIZE)    memory[0x83] = WP_MODE;
-    else if (position < 0x85)           memory[0x84] = WP_MODE;
-    else if (position == 0x85)          memory[0x85] = WP_MODE;
-    else if (position < 0x88)           memory[0x85] = EP_MODE;
+// Status Section
 
-    updatePageStatus();
+void DS2406::clearStatus(void) // copied from DS2506
+{
+    memset(status, value_xFF, STATUS_SIZE);
 }
 
-bool DS2406::getPageProtection(const uint8_t position) const
+uint8_t DS2406::writeStatus(const uint16_t address, const uint8_t value) // copied from DS2506
 {
-    // should be an accurate model of the control bytes
-    if      (position < 1*PAGE_SIZE)
-    {
-        if ((page_protection & 1) != 0) return true;
-    }
-    else if (position < 2*PAGE_SIZE)
-    {
-        if ((page_protection & 2) != 0) return true;
-    }
-    else if (position < 3*PAGE_SIZE)
-    {
-        if ((page_protection & 4) != 0) return true;
-    }
-    else if (position < 4*PAGE_SIZE)
-    {
-        if ((page_protection & 8) != 0) return true;
-    }
-    else if (position == 0x80)
-    {
-        if (((page_protection & (1 + 16)) != 0) || ((page_eprom_mode & 1)) != 0) return true;
-    }
-    else if (position == 0x81)
-    {
-        if (((page_protection & (2 + 16)) != 0) || ((page_eprom_mode & 2)) != 0) return true;
-    }
-    else if (position == 0x82)
-    {
-        if (((page_protection & (4 + 16)) != 0) || ((page_eprom_mode & 4)) != 0) return true;
-    }
-    else if (position == 0x83)
-    {
-        if (((page_protection & (8 + 16)) != 0) || ((page_eprom_mode & 8)) != 0) return true;
-    }
-    else if (position == 0x85)
-    {
-        if ((page_protection & (32+16)) != 0) return true;
-    }
-    else if ((position == 0x86) || (position == 0x87))
-    {
-        if ((page_protection & (64+16)) != 0) return true;
-    }
-    else if (position > 127) // filter the rest
-    {
-        if ((page_protection & 16) != 0) return true;
-    }
-    return false;
+    if (address < STATUS_UNDEF_B1)  status[address] &= value; // writing is allowed only here
+    return status[address];
 }
 
-void DS2406::setPageEpromMode(const uint8_t position)
+uint8_t DS2406::readStatus(const uint16_t address) const // copied from DS2506
 {
-    if      (position < 1*PAGE_SIZE)  memory[0x80] = EP_MODE;
-    else if (position < 2*PAGE_SIZE)  memory[0x81] = EP_MODE;
-    else if (position < 3*PAGE_SIZE)  memory[0x82] = EP_MODE;
-    else if (position < 4*PAGE_SIZE)  memory[0x83] = EP_MODE;
-    updatePageStatus();
+    if (address >= STATUS_SIZE)     return 0xFF;
+    return status[address];
 }
 
-bool DS2406::getPageEpromMode(const uint8_t position) const
+void DS2406::setPageProtection(const uint8_t page) // copied from DS2502
 {
-    if      (position < 1*PAGE_SIZE)
-    {
-        if ((page_eprom_mode & 1) != 0) return true;
-    }
-    else if (position < 2*PAGE_SIZE)
-    {
-        if ((page_eprom_mode & 2) != 0) return true;
-    }
-    else if (position < 3*PAGE_SIZE)
-    {
-        if ((page_eprom_mode & 4) != 0) return true;
-    }
-    else if (position < 4*PAGE_SIZE)
-    {
-        if ((page_eprom_mode & 8) != 0) return true;
-    }
-    return false;
+    if (page < PAGE_COUNT)          status[STATUS_WP_PAGES] &= ~(uint8_t(1<<page));
 }
 
-
-bool DS2406::updatePageStatus(void)
+bool DS2406::getPageProtection(const uint8_t page) const // copied from DS2502
 {
-    page_eprom_mode = 0;
-    page_protection = 0;
+    if (page >= PAGE_COUNT) return true;
+    return ((status[STATUS_WP_PAGES] & uint8_t(1<<page)) == 0);
+}
 
-    if (memory[0x80] == WP_MODE) page_protection |= 1;
-    if (memory[0x81] == WP_MODE) page_protection |= 2;
-    if (memory[0x82] == WP_MODE) page_protection |= 4;
-    if (memory[0x83] == WP_MODE) page_protection |= 8;
+void DS2406::setPageUsed(const uint8_t page) // copied from DS2502
+{
+    if (page < PAGE_COUNT)  status[STATUS_WP_PAGES] &= ~(uint8_t(1<<(page+4)));
+}
 
-    if (memory[0x84] == WP_MODE) page_protection |= 16;
-    if (memory[0x84] == EP_MODE) page_protection |= 16;
+bool DS2406::getPageUsed(const uint8_t page) const // copied from DS2502
+{
+    if (page >= PAGE_COUNT) return true;
+    return ((status[STATUS_WP_PAGES] & uint8_t(1<<(page+4))) == 0);
+}
 
-    if (memory[0x85] == WP_MODE) page_protection |= 32; // only byte x85
-    if (memory[0x85] == EP_MODE) page_protection |= 64+32; // also byte x86 x87
+bool DS2406::setPageRedirection(const uint8_t page_source, const uint8_t page_destin) // copied from DS2502
+{
+    if (page_source >= PAGE_COUNT)  return false; // really available
+    if (page_destin >= PAGE_COUNT)  return false; // virtual mem of the device
 
-    if (memory[0x80] == EP_MODE) page_eprom_mode |= 1;
-    if (memory[0x81] == EP_MODE) page_eprom_mode |= 2;
-    if (memory[0x82] == EP_MODE) page_eprom_mode |= 4;
-    if (memory[0x83] == EP_MODE) page_eprom_mode |= 8;
+    status[page_source + STATUS_PG_REDIR] = (page_destin == page_source) ? uint8_t(0xFF) : ~page_destin; // datasheet dictates this, so no page can be redirected to page 0
+    
     return true;
+}
+
+uint8_t DS2406::getPageRedirection(const uint8_t page) const // copied from DS2502
+{
+    if (page >= PAGE_COUNT) return 0x00;
+    return ~(status[page + STATUS_PG_REDIR]); // TODO: maybe invert this in ReadStatus and safe some Operations? Redirection is critical and often done
+}
+
+// copied from DS2502 and DS2506
+uint8_t DS2406::translateRedirection(const uint8_t source_address) const // TODO: extended read mem description implies that redirection is recursive
+{
+    const uint8_t  source_page    = static_cast<uint8_t >(source_address >> 5);
+    const uint8_t  destin_page    = getPageRedirection(source_page);
+    if (destin_page == 0x00)        return source_address;
+    const uint8_t destin_address  = (source_address & PAGE_MASK) | (destin_page << 5);
+    return destin_address;
 }
--- src/DS2406.h
+++ src/DS2406.h
@@ -8,16 +8,18 @@
 
 #include "OneWireItem.h"
 
+constexpr uint8_t value_xFF { static_cast<uint8_t>(0xFF) };
+
 class DS2406 : public OneWireItem
 {
 private:
 
     // Memory Section
-    static constexpr uint8_t  MEM_SIZE          { 144 };
+    static constexpr uint8_t  MEM_SIZE          { 128 }; // in byte, TODOgba: 144
 
-    static constexpr uint8_t  PAGE_SIZE         { 32 };
-    static constexpr uint8_t  PAGE_COUNT        { MEM_SIZE / PAGE_SIZE };
-    static constexpr uint8_t  PAGE_MASK         { 0b00011111 };
+    static constexpr uint8_t  PAGE_SIZE         { 32 };  // in byte
+    static constexpr uint8_t  PAGE_COUNT        { MEM_SIZE / PAGE_SIZE }; // at the moment = 4
+    static constexpr uint8_t  PAGE_MASK         { PAGE_SIZE - 1 };
 
     static constexpr uint8_t  SCRATCHPAD_SIZE   { 8 };
     static constexpr uint8_t  SCRATCHPAD_MASK   { 0b00000111 };
@@ -26,22 +28,25 @@ private:
     static constexpr uint8_t  REG_ES_ZERO_MASK  { 0b01011000 }; // reads always zero
     static constexpr uint8_t  REG_ES_AA_MASK    { 0b10000000 }; // authorization accepted (data copied to target memory)
 
-    static constexpr uint8_t  WP_MODE           { 0x55 }; // write protect mode
-    static constexpr uint8_t  EP_MODE           { 0xAA }; // eprom mode
-
-    uint8_t memory[MEM_SIZE];
+    uint8_t memory[MEM_SIZE]; // 4 pages of 32 bytes
+    uint8_t scratchpad[SCRATCHPAD_SIZE];
 
     // Pin State Section
     bool pin_state[2];  // sensed input for A and B
     bool pin_latch[2];  // PIO can be set to input (0) or output-to-zero (1)
 
+    // Status Section (copied from DS2502)
+    static constexpr uint8_t    STATUS_SIZE         { 8 }; // in bytes
+    
+    static constexpr uint8_t    STATUS_WP_PAGES     { 0x00 }; // 1 byte -> Page write protection (bit 0-3) and page used status (bit 4-7)
+    static constexpr uint8_t    STATUS_PG_REDIR     { 0x01 }; // 4 byte -> Page redirection
+    static constexpr uint8_t    STATUS_UNDEF_B1     { 0x05 }; // 2 byte -> reserved / undefined
+    static constexpr uint8_t    STATUS_DEVICE       { 0x07 }; // 1 byte -> valid device settings 
 
-    uint8_t scratchpad[SCRATCHPAD_SIZE];
-    uint8_t page_protection;
-    uint8_t page_eprom_mode;
+    uint8_t  status[STATUS_SIZE]; // eprom status bytes
 
-    bool    updatePageStatus(void);
     void    clearScratchpad(void);
+    uint8_t  translateRedirection(uint8_t source_address) const; // react to redirection in status and not available memory
 
 public:
 
@@ -57,11 +62,21 @@ public:
     bool    writeMemory(const uint8_t* source, uint8_t length, uint8_t position = 0);
     bool    readMemory(uint8_t* destination, uint16_t length, uint16_t position = 0) const;
 
-    void    setPageProtection(uint8_t position);
-    bool    getPageProtection(uint8_t position) const;
+    // Status Section
+    void    clearStatus(void);
 
-    void    setPageEpromMode(uint8_t position);
-    bool    getPageEpromMode(uint8_t position) const;
+    uint8_t writeStatus(uint16_t address, uint8_t value);
+    uint8_t readStatus(uint16_t address) const;
+    
+    void    setPageProtection(uint8_t page);
+    bool    getPageProtection(uint8_t page) const; 
+
+    void    setPageUsed(uint8_t page);
+    bool    getPageUsed(uint8_t page) const;
+
+    bool    setPageRedirection(uint8_t page_source, uint8_t page_destin);
+    uint8_t getPageRedirection(uint8_t page) const;
+
 
     // Pin State Section
     bool    setPinState(const uint8_t a_or_b, const bool value)
