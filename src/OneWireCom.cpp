6b2cd00e8d6aa02800a583cfead2028648bfaa5d
rohmannpraktikant1 <praktikant1@rohmann.de>
2023-10-19 14:18:28 +0200

    Function communication was changed. Its adapt to the new onewirecom.

--- src/OneWireCom.cpp
+++ src/OneWireCom.cpp
@@ -16,7 +16,7 @@ void OneWireCom::communicate()
             data = Serial.read();
             if(data == 1){
                 state = 1;
-                Serial.write(4);
+                Serial.write(data+1);
             }
         }else if(state == 1){
             data = Serial.read();
@@ -33,6 +33,7 @@ void OneWireCom::communicate()
                 for(int i = 0; i < 128; i++){
                     Serial.write(readmemory[i]);
                 }
+                
                 state = 0;
             }else if(data == 175){  // 0xAF to Clear Memory
                 // Clear Memory
@@ -40,6 +41,24 @@ void OneWireCom::communicate()
                     m_chip->clearMemory();
                 }
                 Serial.write(170);
+            }else if(data == 20){
+                // read status register
+                for(int i = 0; i < 8; i++){
+                    Serial.write(m_chip->readStatus(i));
+                }
+                state = 0;
+            }else if(data == 30){
+                // PIOA auf 0 überprüfen
+                if(m_chip->readStatus(7) == 95){
+                    Serial.write(55);
+                }
+                state = 0;
+            }else if(data == 40){
+                // PIOB auf 0 überprüfen
+                if(m_chip->readStatus(7) == 63){
+                    Serial.write(55);
+                }
+                state = 0;
             }
 
             
